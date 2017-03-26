9f9c66958c524f790d18ef81ff018b478cef033a
Kirill Meng <kimeng@htwg-konstanz.de>
2017-03-26 23:32:14 +0200

    add directory structure and clone powerline repo

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-cd $(HOME)
+#variables
+
+powerline_package="https://github.com/banga/powerline-shell.git"
+
+#go to home
+cd $HOME
+
+# check if projects dir is there
+
+if [ ! -d "projects" ]
+	then
+		mkdir projects;
+fi
+
+cd projects
+git clone "$powerline_package"
+cd powerline-shell
+cp config.py.dist config.py
+python install.py
+
+#copy the .bashrc stuff into file ...
