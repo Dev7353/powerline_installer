5e6b7bed95874d938486d05e80a3b1a273cdeb85
Kirill Meng <kimeng@htwg-konstanz.de>
2017-03-30 14:57:58 +0200

    add shell commands for .profile dir and shell input to enable changes

--- install.sh
+++ install.sh
@@ -2,22 +2,31 @@
 
 #variables
 
+dir="powerline"
 powerline_package="https://github.com/banga/powerline-shell.git"
+input="function _update_ps1() { PS1="$(~/powerline/powerline-shell.py $? 2> /dev/null)" } if [ "$TERM" != "linux" ]; then PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND" fi"
 
 #go to home
 cd $HOME
 
 # check if projects dir is there
 
-if [ ! -d "projects" ]
+if [ ! -d "$dir" ]
 	then
-		mkdir projects;
+		mkdir "$dir";
 fi
 
-cd projects
+cd "$dir"
 git clone "$powerline_package"
 cd powerline-shell
 cp config.py.dist config.py
 python install.py
 
 #copy the .bashrc stuff into file ...
+if [! -d ".profile"]
+	then
+		mkdir .profile
+fi
+
+echo "$input" >> .profile
+#append bashrc content here
