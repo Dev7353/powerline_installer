3cd6afd9ef4f27a7715a92f6fa2ef226e72faa52
Kirill Meng <kimeng@htwg-konstanz.de>
2017-04-18 18:43:05 +0200

    add final script 	- add config file with shell script

--- install.sh
+++ install.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
-
-#variables
-
-dir="powerline"
 powerline_package="https://github.com/banga/powerline-shell.git"
-input="function _update_ps1() { PS1="$(~/powerline/powerline-shell.py $? 2> /dev/null)" } if [ "$TERM" != "linux" ]; then PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND" fi"
+powerline_fonts="https://github.com/powerline/fonts.git"
+
+cat << "EOF"
+
+______                      _ _                   _____ _          _ _ 
+| ___ \                    | (_)                 /  ___| |        | | |
+| |_/ /____      _____ _ __| |_ _ __   ___ ______\ `--.| |__   ___| | |
+|  __/ _ \ \ /\ / / _ \ '__| | | '_ \ / _ \______|`--. \ '_ \ / _ \ | |
+| | | (_) \ V  V /  __/ |  | | | | | |  __/      /\__/ / | | |  __/ | |
+\_|  \___/ \_/\_/ \___|_|  |_|_|_| |_|\___|      \____/|_| |_|\___|_|_|
+                                                                       
+                                                                       
+                  _           _        _ _                             
+                 (_)         | |      | | |                            
+                  _ _ __  ___| |_ __ _| | | ___ _ __                   
+                 | | '_ \/ __| __/ _` | | |/ _ \ '__|                  
+                 | | | | \__ \ || (_| | | |  __/ |                     
+                 |_|_| |_|___/\__\__,_|_|_|\___|_|
+
+EOF
 
 #go to home
 cd $HOME
 
-# check if projects dir is there
-
-if [ ! -d "$dir" ]
-	then
-		mkdir "$dir";
-fi
-
-cd "$dir"
 git clone "$powerline_package"
 cd powerline-shell
 cp config.py.dist config.py
 python install.py
 
-#copy the .bashrc stuff into file ...
-if [! -d ".profile"]
-	then
-		mkdir .profile
-fi
+#go home
+cd $HOME
 
-echo "$input" >> .profile
-#append bashrc content here
+#copy the .bashrc stuff into file ...
+cat config >> .bashrc
+
+#install fonts
+git clone "$powerline_fonts"
+cd fonts
+./install.sh
+cd  $HOME
+
+echo "Now setup a profile an enjoy the powerline-shell"
